c31599bcf1312ddc606e662899afd66ace50fc3b
Alexander Casal <alexandericasal@gmail.com>
2017-10-30 21:25:42 -0600

    Updated earthquake list item UI
    - Added colored circle drawable
    - Set magnitude background to circle drawable
    - Change circle drawable color based on magnitude value
    - Updated fonts/colors for earthquake list items

--- app/src/main/java/com/example/android/quakereport/model/EarthquakeListAdapter.kt
+++ app/src/main/java/com/example/android/quakereport/model/EarthquakeListAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.android.quakereport.model
 
 import android.content.Context
+import android.graphics.drawable.GradientDrawable
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 import com.example.android.quakereport.R
+import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 
 /**
@@ -58,8 +61,10 @@ class EarthquakeListAdapter(val context: Context, val earthquakes: ArrayList<Ear
             val dateFormatter = SimpleDateFormat("LLL dd, yyyy")
             val timeFormatter = SimpleDateFormat("h:mm a")
             val locationParts = getLocationParts(earthquake.location)
+            val magnitudeCircle = quakeMagnitude.background as GradientDrawable
 
-            quakeMagnitude.text = earthquake.magnitude.toString()
+            magnitudeCircle.setColor(getMagnitudeColor(earthquake.magnitude))
+            quakeMagnitude.text = formatMagnitude(earthquake.magnitude)
             quakeLocOffset.text = locationParts[0]
             quakeLocation.text = locationParts[1]
             quakeDate.text = dateFormatter.format(earthquake.time)
@@ -80,6 +85,26 @@ class EarthquakeListAdapter(val context: Context, val earthquakes: ArrayList<Ear
                         "Near the",
                         location
                 )
+            }
+        }
+
+        private fun formatMagnitude(mag: Float): String {
+            val formatter = DecimalFormat("0.0")
+            return formatter.format(mag)
+        }
+
+        private fun getMagnitudeColor(mag: Float): Int {
+            return when(mag) {
+                in 0..2 -> ContextCompat.getColor(context, R.color.magnitude1)
+                in 2..3 -> ContextCompat.getColor(context, R.color.magnitude2)
+                in 3..4 -> ContextCompat.getColor(context, R.color.magnitude3)
+                in 4..5 -> ContextCompat.getColor(context, R.color.magnitude4)
+                in 5..6 -> ContextCompat.getColor(context, R.color.magnitude5)
+                in 6..7 -> ContextCompat.getColor(context, R.color.magnitude6)
+                in 7..8 -> ContextCompat.getColor(context, R.color.magnitude7)
+                in 8..9 -> ContextCompat.getColor(context, R.color.magnitude8)
+                in 9..10 -> ContextCompat.getColor(context, R.color.magnitude9)
+                else -> ContextCompat.getColor(context, R.color.magnitude10plus)
             }
         }
     }
