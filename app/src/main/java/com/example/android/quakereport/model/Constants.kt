0286713af16d253d64afb70fb62266cd15a6d735
Alexander Casal <alexandericasal@gmail.com>
2017-11-01 22:55:25 -0600

    Created Constants file to store application wide constant values
    - Added url to USGS earthquakes endpoint

---
+++ app/src/main/java/com/example/android/quakereport/model/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.android.quakereport.model
+
+const val URL_USGS_EARTHQUAKE_API = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10"
