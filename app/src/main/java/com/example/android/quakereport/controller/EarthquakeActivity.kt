3e0184a60ac6e6bc456b790c3e0954855a6598d5
Alexander Casal <alexandericasal@gmail.com>
2017-11-01 23:37:34 -0600

    Modified LoadEarthquakeAsyncTask to use WeakReference to Activity to prevent leaking the Activity

--- app/src/main/java/com/example/android/quakereport/controller/EarthquakeActivity.kt
+++ app/src/main/java/com/example/android/quakereport/controller/EarthquakeActivity.kt
@@ -6,11 +6,14 @@ import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ListView
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.example.android.quakereport.R
 import com.example.android.quakereport.model.Earthquake
 import com.example.android.quakereport.model.URL_USGS_EARTHQUAKE_API
 import kotlinx.android.synthetic.main.earthquake_activity.*
+import java.lang.ref.WeakReference
 
 class EarthquakeActivity : AppCompatActivity() {
 
@@ -45,14 +48,17 @@ class EarthquakeActivity : AppCompatActivity() {
             }
         }
 
-        LoadEarthquakesAsyncTask().execute(URL_USGS_EARTHQUAKE_API)
+        LoadEarthquakesAsyncTask(WeakReference(this)).execute(URL_USGS_EARTHQUAKE_API)
     }
 
-    private inner class LoadEarthquakesAsyncTask : AsyncTask<String, Void, List<Earthquake>>() {
+    private class LoadEarthquakesAsyncTask(val activity: WeakReference<EarthquakeActivity>)
+        : AsyncTask<String, Void, List<Earthquake>>() {
 
         override fun onPreExecute() {
-            progress_load_earthquakes.visibility = View.VISIBLE
-            list_earthquakes.isEnabled = false
+            activity.get()?.findViewById<ProgressBar>(R.id.progress_load_earthquakes)
+                    ?.visibility = View.VISIBLE
+            activity.get()?.findViewById<ListView>(R.id.list_earthquakes)
+                    ?.isEnabled = false
         }
 
         override fun doInBackground(vararg endpoint: String): List<Earthquake> {
@@ -60,8 +66,10 @@ class EarthquakeActivity : AppCompatActivity() {
         }
 
         override fun onPostExecute(earthquakes: List<Earthquake>) {
-            progress_load_earthquakes.visibility = View.GONE
-            list_earthquakes.isEnabled = true
+            activity.get()?.findViewById<ProgressBar>(R.id.progress_load_earthquakes)
+                    ?.visibility = View.GONE
+            activity.get()?.findViewById<ListView>(R.id.list_earthquakes)
+                    ?.isEnabled = true
         }
     }
 }
