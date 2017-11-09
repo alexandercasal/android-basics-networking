bb86ddc22cb5a96a5b0426cd736a42df8047f758
Alexander Casal <alexandericasal@gmail.com>
2017-11-08 23:29:27 -0700

    Added preview of selected magnitude to Minimum Magnitude item
    - Implemented Preference.OnPreferenceChangeListener to update
    preference item summary

--- app/src/main/java/com/example/android/quakereport/controller/SettingsActivity.kt
+++ app/src/main/java/com/example/android/quakereport/controller/SettingsActivity.kt
@@ -2,7 +2,9 @@ package com.example.android.quakereport.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.Preference
 import android.preference.PreferenceFragment
+import android.preference.PreferenceManager
 import com.example.android.quakereport.R
 
 class SettingsActivity : AppCompatActivity() {
@@ -12,11 +14,27 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
     }
 
-    class EarthquakePreferenceFragment : PreferenceFragment() {
+    class EarthquakePreferenceFragment : PreferenceFragment(), Preference.OnPreferenceChangeListener {
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             addPreferencesFromResource(R.xml.settings_main)
+
+            val minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key))
+            bindPreferenceSummaryToValue(minMagnitude)
+        }
+
+        override fun onPreferenceChange(preference: Preference, value: Any): Boolean {
+            val stringValue = value.toString()
+            preference.setSummary(stringValue)
+            return true
+        }
+
+        private fun bindPreferenceSummaryToValue(preference: Preference) {
+            preference.setOnPreferenceChangeListener(this)
+            val preferences = PreferenceManager.getDefaultSharedPreferences(preference.context)
+            val preferenceString = preferences.getString(preference.key, "")
+            onPreferenceChange(preference, preferenceString)
         }
     }
 }
